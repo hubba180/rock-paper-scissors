b90905ceb7e87f1ea1246d38982f1d5611b5019d
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-15 14:07:01 -0700

    rename files to CamelCase and past first test
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>

---
+++ RockPaperScissors.tests/tests/GameObject.tests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System; 
+using RockPaperScissors;
+
+namespace RockPaperScissors.tests 
+{
+  [TestClass]
+  public class RockPaperScissorsTests 
+  {
+    [TestMethod]
+    public void GameObjectConstructor_CreatesInstanceOfGameObject_GameObject()
+    {
+      
+    }
+  }
+}
